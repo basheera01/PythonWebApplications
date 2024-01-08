48b8518fc0b5f069947ac5a5038f46a0aa16116f
Basheera shaik <134625274+basheera01@users.noreply.github.com>
2024-01-08 12:02:01 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -4,7 +4,7 @@ st.header("Highest Peak Mounts")
 col1,col2,col3=st.columns(3)
 with col1:
   st.subheader("Everest")
-  st.image("./everest.jpg",caption="Mount Everest",width=300,use_column_width=True)
+  st.image("everest.jpg",caption="Mount Everest",width=300,use_column_width=True)
   st.write("Mount Everest, standing at 29,032 feet (8,848 meters), is the world's highest peak, located in the Himalayas on the border of Nepal and Tibet.")
 with col1:
   st.subheader("K2")
