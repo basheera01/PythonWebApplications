191c6c538a0c734a70a56a482f46e020de47d942
Basheera shaik <134625274+basheera01@users.noreply.github.com>
2024-01-08 12:20:26 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.set_page_config(page_title='Mounts')
 st.header("Highest Peak Mounts")
-col1,col2=st.columns(2)
+col1,col2,col3=st.columns(3)
 with col1:
   st.subheader("Everest")
   st.image("everest.jpg",caption="Mount Everest",width=300,use_column_width=True)
@@ -10,6 +10,5 @@ with col2:
   st.subheader("K2")
   st.image("k2.jpg",caption="Mount K2",width=300,use_column_width=True)
   st.write("K2, the second-highest mountain at 28,251 feet (8,611 meters), is renowned for its formidable challenges and is part of the Karakoram Range on the China-Pakistan border.")
-col3=st.columns(1)
 with col3:
   st.image("mount.gif",width=500,use_column_width=True)
